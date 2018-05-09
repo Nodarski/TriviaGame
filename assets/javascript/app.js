0e9c8cb6d5d3a839db83ee9dd7a4fe8410d5776e
Nodarski <nodarlaw@gmail.com>
2018-05-09 16:00:37 -0600

    working code added to app.js. not pretty but it works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,349 @@
-console.log("hello");
+var guessedCorrect = 0;
+var guessedInCorrect = 0;
+var currentQuestion = -1;
+var questionsLeft = 20;
+var correctAns;
+var counter = 30;
+var whyThis= [".ansOne",".ansTwo",".ansThree",".ansFour"];
+//theQuestions[i++].correct
+var theQuestions =[ {
+            questions : "The question 1goes here?" ,
+            options : ["no","no","nope","yesss"],
+            correct : 3
+        }, {
+            questions : "The question 2goes here?",
+            options : ["nahh","no","yisss","i dont 2 so.."],
+            correct : 2
+        }, {
+            questions : "The question 3goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 4goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 5goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 6goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 7goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 8goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 9goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 10goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 11 goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 12 goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 13goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 14goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 15goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 16goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 17goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 18goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 19goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }, {
+            questions : "The question 20goes here?",
+            options : ["wrong! SAD","yup","nope","i dont think so.."],
+            correct : 1
+        }
+
+];
 
 
-// LOOPING COUNTDOWN
-// LOOPING COUNTDOWN
-// LOOPING COUNTDOWN
 
-//How much time is given
-//How much time is given
-var counter = 3;
 
-//inser the time remaining into the div thingy
-var divTimeInsert = function() {
-    $("#countDwn").text(counter);
+                    // i would like to make the div class=boxes filled with the objects in the array.
+                    //the boxes will have .text(theQuestions[0++].options[0++])
+
+                    //    the question div will contain .text(theQuestions[0++].questions)
+
+
+
+
+
+
+
+
+
+
+                    // LOOPING COUNTDOWN
+                    // LOOPING COUNTDOWN
+                    // LOOPING COUNTDOWN
+
+function countDown(){
+
+
+                //How much time is given
+                //How much time is given
+
+        //insert the time remaining into the div thingy
+
+    var divTimeInsert = function() {
+        $("#countDwn").text(counter);
+    };
+
+            // clearInterval(interval);
+
+    var interval = setInterval(function() {
+        counter--;
+        divTimeInsert();
+
+                //counter at -1, to make sure player sees a 0
+        if (counter == -1) {
+
+            // what to do when time runs out!console.log('yo');
+            questionsLeft--;
+            guessedInCorrect++;
+            questSelect();
+            $('#timeOut').css("display","flex");
+
+
+            setTimeout(function(){
+                $('#timeOut').css("display","none");
+                counter = 30;
+                divTimeInsert();       
+
+        }, 3000);
+        }
+    }, 1000);
+
+// Function countDown();  ~line 113
+};
+// Function countDown();  ~line 113
+
+
+
+function startGame(){
+
+    $('#startPage').slideUp();
+    console.log("hello");
+    questSelect();
+    countDown();
+
 };
 
-// clearInterval(interval);
 
-var interval = setInterval(function() {
-    counter--;
-    divTimeInsert();
-    
-    //counter at -1, to make sure player sees a 0
-    if (counter == -1) {
 
-        // what to do when time runs out!
-        console.log('yo');
-        counter = 3;
-        divTimeInsert();       
+function questSelect(){
+    if (questionsLeft==0) {
+                                    //game is over!!
+                                    //game is over!!
+                                    //Winner screen!!
+        alert("game over");
+        $("#winnerScreen").text("Good Job! you have beaten the game with: " + guessedCorrect + " questions guessed correctly. And : " + guessedInCorrect + " guessed incorrectly!");
+        $("#winnerScreen").css("display","flex")
+
+        setTimeout(function(){
+            $('#winnerScreen').css("display","none");
+            counter = 30;
+            questionsLeft = 20;
+            currentQuestion = -1;
+
+        }, 10000);
+
+
+
+        
+
+    }   
+
+    else{
+        currentQuestion++;
+        correctAns = theQuestions[currentQuestion].correct;
+        $(".theQuestion").text(theQuestions[currentQuestion].questions);
+
+        
+        for (i = 0; i < theQuestions[currentQuestion].options.length; i++) { 
+
+            $(whyThis[i]).text(theQuestions[currentQuestion].options[i]);
+
+        }
     }
-}, 1000);
+};
+
+
+
+
+
+$("#introText").on("click",".ansOne",function(){
+
+    var ansOne=whyThis.indexOf(".ansOne");
+
+    if (correctAns==ansOne){
+
+        $('#rightPage').css("display","flex");
+        guessedCorrect++;
+        questionsLeft--;
+        console.log("hello");
+        questSelect();
+        setTimeout(function(){
+            $('#rightPage').css("display","none");
+            counter = 30;
+
+        }, 3000);
+
+    }
+
+    else{
+        $('#wrongPage').css("display","flex");
+        guessedInCorrect++;
+        questionsLeft--;
+        questSelect();
+
+        setTimeout(function(){
+            $('#wrongPage').css("display","none");
+            counter = 30;
+
+        }, 3000);
+
+    }
+});
+
+$("#introText").on("click",".ansTwo",function(){
+
+    var ansOne=whyThis.indexOf(".ansTwo");
+
+    if (correctAns==ansOne){
+
+        $('#rightPage').css("display","flex");
+        guessedCorrect++;
+        questionsLeft--;
+        console.log("hello");
+        questSelect();
+        setTimeout(function(){
+            $('#rightPage').css("display","none");
+            counter = 30;
+
+        }, 3000);
+
+    }
+
+    else{
+        $('#wrongPage').css("display","flex");
+        guessedInCorrect++;
+        questionsLeft--;
+        questSelect();
+
+        setTimeout(function(){
+            $('#wrongPage').css("display","none");
+            counter = 30;
+
+        }, 3000);
+
+    }    
+});
+
+$("#introText").on("click",".ansThree",function(){
+
+    var ansOne=whyThis.indexOf(".ansThree");
+
+    if (correctAns==ansOne){
+
+        $('#rightPage').css("display","flex");
+        guessedCorrect++;
+        questionsLeft--;
+        console.log("hello");
+        questSelect();
+        setTimeout(function(){
+            $('#rightPage').css("display","none");
+            counter = 30;
+
+        }, 3000);
+
+    }
+
+    else{
+        $('#wrongPage').css("display","flex");
+        guessedInCorrect++;
+        questionsLeft--;
+        questSelect();
+
+        setTimeout(function(){
+            $('#wrongPage').css("display","none");
+            counter = 30;
+
+        }, 3000);
+
+    }    
+});
+
+$("#introText").on("click",".ansFour",function(){
+
+    var ansOne=whyThis.indexOf(".ansFour");
+
+    if (correctAns==ansOne){
+
+        $('#rightPage').css("display","flex");
+        guessedCorrect++;
+        questionsLeft--;
+        console.log("hello");
+        questSelect();
+        setTimeout(function(){
+            $('#rightPage').css("display","none");
+            counter = 30;
+
+        }, 3000);
+
+    }
+
+    else{
+        $('#wrongPage').css("display","flex");
+        guessedInCorrect++;
+        questionsLeft--;
+        questSelect();
+
+        setTimeout(function(){
+            $('#wrongPage').css("display","none");
+            counter = 30;
+
+    }, 3000);
+
+
+}
+});
+
+
+
+
+
